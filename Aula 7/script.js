1461f7d59dead48afdc5b5c30fa6d49686b7b5ca
Gerônimo de Araujo Franco <106390580+FrancoHTML@users.noreply.github.com>
2022-06-30 09:35:54 -0300

    Create script.js

---
+++ Aula 7/script.js
@@ -0,0 +1,24 @@
+let botao_enviar = document.querySelector('#btn-enviar')
+
+botao_enviar.addEventListener('click', enviarResposta)
+
+function enviarResposta(){
+    let q1_1  = document.querySelector('#q1_1')
+    let q1_2  = document.querySelector('#q1_2')
+    let q1_3  = document.querySelector('#q1_3')
+    let q1_4  = document.querySelector('#q1_4')
+    let q1_5  = document.querySelector('#q1_5')
+}
+
+    if (q1.checked){
+
+    } else if (q1_1.checked){
+
+    } else if (q1_2.checked){
+
+    } else if (q1_3.checked){
+
+    } else if (q1_4.checked){
+
+    } else if (q1_5.checked){
+}
