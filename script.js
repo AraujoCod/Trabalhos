6745c6626c74bc4ae1f705b9f5a6018a9477e35c
Gerônimo de Araujo Franco <106390580+FrancoHTML@users.noreply.github.com>
2022-06-29 22:05:10 -0300

    Comidas Brasileiras

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-let usuarios = []
+const selecionarProdutos = function (opcao) {
+    let resultado = document.getElementById('resultado')
 
-const addUsuario = () => {
-    let nome = document.getElementById('nome').value
-    let email = document.getElementById('email').value
-    let senha = document.getElementById('senha').value
+    switch (opcao) {
 
-   if (localStorage.getItem('usuarios') != null){
-    usuarios = JSON.parse(localStorage.getItem('usuarios'))
-
-   }
-
-    usuarios.push([nome, email, senha])
-    localStorage.setItem('usuarios', JSON.stringify(usuarios))
-}
-
-const listUsuarios = () => {
-    let tbody = document.getElementById('tabela-linha')
-    if (localstorage.getItem('usuarios') != null){
-    usuarios = JSON.parse(localStorage.getItem('usuarios'))
-    usuarios.forEach(usuario => {
-        tbody.innerHTML += "<tr> <td>"+usuario[0] +"</td> <td>"+usuario[1]+"</td> </tr>"
-        
-    });
-        
+        case 'Arroz Carreteiro':
+            console.log('Opção selecionada é Arroz Carreteiro')
+            resultado.innerHTML = "Comida tipica feita pelos Gaúchos!"
+            break;
+        case 'Tacacá':
+            console.log('Opção selecionada é Tacacá')
+            resultado.innerHTML = "Essa comida é da região Norte do Brasil!"
+            break
+        case 'Pizza':
+            console.log('Opção selecionada é Pizza')
+            resultado.innerHTML = "A pizza não é do Brasil, foi criada pela Itália!"
+            break;
+        case 'Baião de Dois':
+            console.log('Opção selecionada é Baião de Dois')
+            resultado.innerHTML = "Comida tipica da região nordestina!"
+            break;
+        case 'Mujica de Peixe':
+            console.log('Opção selecionada é Mujica de Peixe')
+            resultado.innerHTML = "Essa comida é da região Norte do Brasil!"
+            break;
+        case 'Arroz com Pequi':
+            console.log('Opção selecionada é Arroz com Pequi')
+            resultado.innerHTML = "Essa comida é da região Centro-oeste do Brasil!"
+            break;
+        case 'Pão de Queijo':
+            console.log('Opção selecionada é Pão de Queijo')
+            resultado.innerHTML = "Essa comida é da região Sudeste do Brasil!"
+            break;
+        case 'Parrillada':
+            console.log('Opção selecionada é Parrillada')
+            resultado.innerHTML = "Aclamado como paixão nacional do Uruguai, esta comida não é tipica do Brasil!"
+            break;
+        case 'Wurst':
+            console.log('Opção selecionada é Wurst')
+            resultado.innerHTML = "Esta comida não é da região do Brasil, ela foi criada na Alemanha!!"
+            break;
+        default:
+            console.log('Opção selecionada é Lámen');
+            resultado.innerHTML = "Esta comida não é brasileira e sim japonesa!!"
+            break;
     }
-    tbody.innerHTML = "Gerônimo</td> <td>geronimo_franco@estudante.sesisenai.org.br" 
 }
